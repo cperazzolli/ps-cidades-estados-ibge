3925bf09eaf28dd8a4c817c73d3388c4324f63ed
Claudinei Perazzolli <claudineii.perazzolli@gmail.com>
2025-03-22 10:36:49 -0300

    Ajuste

--- src/main/java/br/com/ibge/cad/client/estados/EstadosClient.java
+++ src/main/java/br/com/ibge/cad/client/estados/EstadosClient.java
@@ -6,34 +6,37 @@ import br.com.ibge.cad.client.ClientResponse;
 import br.com.ibge.cad.client.WebClientTemplate;
 import br.com.ibge.cad.config.IbgeClientConfig.IbgeClientProperties;
 import br.com.ibge.cad.domain.EstadoResponse;
+
+import javax.net.ssl.SSLException;
+
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
-import javax.net.ssl.SSLException;
-
 @Component
 public class EstadosClient extends WebClientTemplate {
+
     protected EstadosClient(@IbgeClientProperties final BaseClientProperties properties) throws SSLException {
         super(properties);
     }
 
-    public ClientResponse<EstadoResponse,ClientError> find(){
+    public ClientResponse<EstadoResponse, ClientError> find() {
 
         try {
             final EstadoResponse estadosResponse = this.webClient.get()
                     .uri(uriBuilder -> uriBuilder.path("/api/v1/localidades/estados")
-                    .build())
+                            .build())
                     .retrieve()
-                    .bodyToMono(new ParameterizedTypeReference<EstadoResponse>(){})
+                    .bodyToMono(new ParameterizedTypeReference<EstadoResponse>() {
+                    })
                     .timeout(timeoutInSeconds())
                     .retryWhen(retryBackoffSpec())
                     .onErrorResume(WebClientResponseException.class, fallbackResponseException())
                     .block();
 
             return ClientResponse.withSuccess(estadosResponse);
-        } catch(final WebClientResponseException ex) {
-            return ClientResponse.withError(ClientError.with(ex.getMessage(),ex.getStatusCode()));
+        } catch (final WebClientResponseException ex) {
+            return ClientResponse.withError(ClientError.with(ex.getMessage(), ex.getStatusCode()));
         }
 
     }
